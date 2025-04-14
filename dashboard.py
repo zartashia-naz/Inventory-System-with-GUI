b7702c1d55cd4c759189f931a4bca9afdc10f9cd
zartashia.nazz@gmail.com <66721567+m-nouman-ashraf@users.noreply.github.com>
2025-04-14 23:06:44 +0500

    test commit

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ class IMS:
         self.root.title("Inventory Management System")
 
         self.logo=PhotoImage(file="C:\\Users\\BilalQA\\Desktop\\inventory management system\\img logo2.png")
-        lbl_title=Label(self.root,text="INVENTORY MANAGMENT SYSTEM",image=self.logo,compound=LEFT,padx=10,font=("Arial",40,"bold"),bg="purple",fg="white",anchor="w").place(x=0,y=0,relwidth=1,height=70)
+        lableTitle=Label(self.root,text="INVENTORY MANAGMENT SYSTEM",image=self.logo,compound=LEFT,padx=10,font=("Arial",40,"bold"),bg="purple",fg="white",anchor="w").place(x=0,y=0,relwidth=1,height=70)
 
         logout_btn=Button(self.root,text="LOGOUT",font=("Arial",15),bg="light yellow",fg="black",cursor="hand2").place(x=1240,y=20,height=25)
         lb12=Label(self.root,text="Welcome to Inventory Management System",font=("Arial",25),bg="light gray",fg="white",justify=CENTER).place(x=0,y=70,relwidth=1,height=40)
